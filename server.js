598276b8ee5e830deb16015a7ac6b846e05b0083
unknown <raulsdev@gmail.com>
2020-09-02 13:45:00 -0500

    added port for heroku

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/profile/:id',(req,res) => {profile.handleProfileGet(req,res,db)})
 app.put('/image', (req,res) => {image.handleImage(req,res,db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(3000, () => {
-    console.log('app is runnung in port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is runnung in port ${process.env.PORT}`);
 });
 

135d6ac0e2f3844d17f81f0ce6c1fc8d9c35a4cf
unknown <raulsdev@gmail.com>
2020-09-03 19:41:40 -0500

    fixed typo

--- controllers/image.js
+++ controllers/image.js
@@ -15,16 +15,14 @@ const handleApiCall = (req,res) => {
 }
 
 const handleImage = (req, res,db) => {
-const {id} = req.params;
+const {id} = req.body;
 db('users').where('id', '=', id)
     .increment('entries', 1)
     .returning('entries')
     .then(entries => {
         res.json(entries[0]);
     })
-    
     .catch(err => res.status(400).json('unable to get entries'))
-    console.log(err);
 }
 
 module.exports = {
